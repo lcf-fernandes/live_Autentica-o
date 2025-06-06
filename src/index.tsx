2b11c0a547ba49fdd01fed374be3d9928a732f5e
Leandro <leandrowebmaster@gmail.com>
2025-06-06 16:28:04 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -565,7 +565,6 @@ const path1 = "scA0.toogles.login";
         padding: 20,
         alignItems: "center",
         justifyContent: "center",
-        margimBottom: 10,
       }}
     >
       <RN.TextInput
