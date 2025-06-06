60ffaf0a26c94ba31193ca87c813571497d95077
Leandro <leandrowebmaster@gmail.com>
2025-06-06 13:10:10 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -540,7 +540,7 @@ xmlns="http://www.w3.org/2000/svg"
 
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
- arrFunctions: [() => ["scA0.toogles.login", "==", false ]]
+ arrFunctions: [() => ["scA0.toogles.login", "==", true ]]
  , trigger: 'on listen'
 }})],            childrenItems:[() => {
   const [userName, setUserName] = React.useState("");
